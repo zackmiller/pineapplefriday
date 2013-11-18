db7026b6720d9b7e49e6731ee849f25d95b77e12
Zack Miller <zack@zackmiller.ca>
2013-11-18 16:09:59 -0400

    install Ostrich Sans

--- config.rb
+++ config.rb
@@ -8,12 +8,13 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+http_fonts_path = http_path + "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
